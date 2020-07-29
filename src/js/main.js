da083838eec86552e5b04e25cf0e6edaad6b441f
Miklovsh <miklosh.vik@gmail.com>
2020-07-29 15:00:21 +0300

    Зробив на jquery фільтрований список

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,29 @@ $(".about__accordeon dd").hide().prev().click(function() {
 	$(this).parents(".about__accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
 	$(this).next().not(":visible").slideDown().prev().addClass("active");
 });
+
+
+$(function() {
+
+    let filter = $("[data-filter]");
+
+    filter.on("click", function(event) {
+        event.preventDefault();
+
+        let cat = $(this).data('filter');
+
+        if(cat == 'all') {
+            $("[data-cat]").removeClass("hide");
+        } else {
+            $("[data-cat]").each(function() {
+                let dishCat = $(this).data('cat');
+
+                if(dishCat != cat) {
+                    $(this).addClass('hide');
+                } else {
+                    $(this).removeClass('hide');
+                }
+            });
+        }
+    });
+});
